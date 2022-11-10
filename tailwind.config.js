26d054c2fe9bb523f7953b5f5856d07561dc6073
Val_M <v.mischenko90@gmail.com>
2022-11-10 23:01:10 +0200

    added fonts, star svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+       'sans': ['Proxima Nova', 'Proxima Nova', 'Proxima Nova']
+      },
+      colors: {
+        'page-bg': '#E6E9F2',
+        'card-mobile-bg': '#EFF0F5',
+        'card-desktop-bg': '#FFFFFF',
+        'text-primary': '#3A4562',
+        'text-secondary': '#878D9D'
+      },
+      maxWidth: {
+        'card-mobile': '396px',
+        'card-desktop': '1440px',
+      },
+      width: {
+        'photo-mobile': '66px',
+        'photo-desktop': '85px',
+      },
+      height: {
+        'card-mobile': '206px',
+        'card-desktop': '164px',
+        'photo-mobile': '66px',
+        'photo-desktop': '85px',
+      }
+    },
   },
   plugins: [],
 };
