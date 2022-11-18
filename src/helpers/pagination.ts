27c23808ee09395e2cf3bcf24dbc899b1cf78191
Val_M <v.mischenko90@gmail.com>
2022-11-18 12:25:47 +0200

    fix pagination

--- src/helpers/pagination.ts
+++ src/helpers/pagination.ts
@@ -5,12 +5,14 @@ const range = (start: number, end: number) =>
 const pagination = (count: number) => (page: string, total: string) => {
   const start = max(1, min(+page - floor((count - 3) / 2), +total - count + 2));
   const end = min(+total, max(+page + floor((count - 2) / 2), count - 1));
-  const mixedArray = [
-    ...(start > 2 ? [1, '...'] : start > 1 ? [1] : []),
-    ...range(start, end + 1),
-    ...(end < +total - 1 ? ['...', total] : end < +total ? [total] : []),
-  ];
-  return Array.from(mixedArray, String);
+  return Array.from(
+    [
+      ...(start > 2 ? [1, '...'] : start > 1 ? [1] : []),
+      ...range(start, end + 1),
+      ...(end < +total - 1 ? ['...', total] : end < +total ? [total] : []),
+    ],
+    String,
+  );
 };
 
 export { pagination };
